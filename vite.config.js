a2d15267c88e1053d06e092e77f4c362cdf1840d
aya-soghayyer <128791822+aya-soghayyer@users.noreply.github.com>
2025-03-16 11:36:28 +0200

    trying deploying

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // Bind to all interfaces
+    host: true, // Bind to all interfaces
     port: 5173,
   }
 })

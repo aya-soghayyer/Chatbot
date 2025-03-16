2b7a2a41aaa0452f72fc7065883b2cb78721712c
aya-soghayyer <128791822+aya-soghayyer@users.noreply.github.com>
2025-03-16 10:23:15 +0200

    update(navbar): making responsive navbar

--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -5,15 +5,15 @@ import WelcomeHeader from "../Compononts/Header/WelcomeHeader"; // Fixed typo in
 function Welcome() {
   return (
     <>
-      <div className="bg-Primary min-h-screen w-full">
-        <div className="flex flex-col md:flex-row bg-primary items-center justify-between px-4 sm:px-6 lg:px-8">
-          <div className="w-full  md:w-auto -mt-4 md:-mt-8 lg:-mt-16">
+      <div className="bg-Primary w-full">
+        <div className="flex flex-col md:flex-row bg-primary items-center justify-between px-4 sm:px-6 lg:px-16">
+          <div className="w-full  md:w-auto -mt-4 md:-mt-8 lg:scroll-mt-px">
             <Circle />
           </div>
-          <div className="w-full z-10 md:w-auto my-4 md:my-0">
+          <div className="w-screen z-10 md:w-auto md:my-0">
             <WelcomeHeader />
           </div>
-          <div className="w-full md:w-auto hidden md:block">
+          <div className="w-full md:w-auto md:block">
             <Circle />
           </div>
         </div>

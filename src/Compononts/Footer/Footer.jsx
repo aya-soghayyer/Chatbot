ca120fd733ffca5c27e5d0896bfe7db5a34c3e2d
aya-soghayyer <128791822+aya-soghayyer@users.noreply.github.com>
2025-03-30 14:29:45 +0200

    update(footer): add padding for mobile screens

--- src/Compononts/Footer/Footer.jsx
+++ src/Compononts/Footer/Footer.jsx
@@ -3,7 +3,7 @@
 function Footer() {
   return (
     <>
-      <div className="grid justify-center gap-1 items-center md:flex md:justify-between md:items-center md:h-14 md:px-16  ">
+      <div className="grid justify-center gap-1 p-3 items-center md:flex md:justify-between md:items-center md:h-14 md:px-16  ">
         <span className="z-10 justify-self-center"> © 2025 – MiLo Chatbot </span>
         <div className="underline underline-offset-1 justify-self-center">
           <a href="https://portal.hebron.edu/Default.aspx">

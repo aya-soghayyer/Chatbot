efba6456a2c15b326dd6e5c10040e90e7a722f4e
aya-soghayyer <128791822+aya-soghayyer@users.noreply.github.com>
2025-04-07 21:59:23 +0200

    update(notFound): make it responsive

--- src/Compononts/Header/NotFoundHeader.jsx
+++ src/Compononts/Header/NotFoundHeader.jsx
@@ -1,13 +1,17 @@
-import notFound from '../../assets/images/notFound.svg'
+import notFound from "../../assets/images/notFound.svg";
 
 function NotFoundHeader() {
-    return (
-        <>
-            <div className=' w-[50%] z-10 '>
-                <img className=' ml-[10%] mt-[10%] mb-[15%]' src={notFound} alt="Not Found photo" />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="w-full md:w-[50%] mt-[50%] md:mb-[0] md:mt-[5%] px-6">
+        <img
+          className="mt-[10%] md:mt-[5%] md:mb-[10%]"
+          src={notFound}
+          alt="Not Found Photo"
+        />
+      </div>
+    </>
+  );
 }
 
-export default NotFoundHeader
+export default NotFoundHeader;
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -5,14 +5,16 @@ import NotFoundHeader from "../Compononts/Header/NotFoundHeader";
 function NotFound() {
   return (
     <>
-      <div className=" flex -mb-8">
-        <div className="  flex justify-between">
-          <div className="-mt-16 ">
-            <Circle />
-          </div>
+      <div className="bg-primary items-center h-screen md:flex md:justify-center md:items-center 2xl:h-screen 2xl:flex 2xl:justify-center 2xl:items-center relative">
+        <div className="absolute right-3 -top-12 md:-mt-28 md:right-[63rem] 2xl:-left-64 2xl:-top-24">
+          <Circle className="bg-red-500" />
         </div>
-        <NotFoundHeader />
-        <Circle />
+        <div className="2xl:flex 2xl:justify-center md:my-7 md:flex md:justify-center md:w-11/12 2xl:w-11/12 2xl:-mt-16 z-10 relative">
+          <NotFoundHeader />
+        </div>
+        <div className="absolute md:-z-0 hidden md:inline-flex md:top-16 md:right-32 2xl:top-40 2xl:right-11">
+          <Circle />
+        </div>
       </div>
     </>
   );
